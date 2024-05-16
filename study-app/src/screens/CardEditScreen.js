04b1dd33826ba487914b3d14a9a99486ecd9c99e
Kelson Ameida <kelson.victr@gmail.com>
2024-05-16 20:10:29 -0300

    cardeditscreen

--- study-app/src/screens/CardEditScreen.js
+++ study-app/src/screens/CardEditScreen.js
@@ -2,6 +2,8 @@ import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
 import React, { useContext, useState, useEffect } from 'react'
 import { Picker } from '@react-native-picker/picker'
 import StudyCardsContext from '../contexts/StudyCardsContext'
+import DateTimePickerModal from 'react-native-modal-datetime-picker'
+
 
 const CardEditScreen = ({ route, navigation }) => {
 
@@ -12,17 +14,20 @@ const CardEditScreen = ({ route, navigation }) => {
     const [title, setTitle] = useState(card.title || '')
     const [notes, setNotes] = useState(card.notes || '')
     const [status, setStatus] = useState(card.status || 'backlog')
+    const [dueDate, setDueDate] = useState(card.dueDate ? new Date(card.dueDate) : new Date())
+    const [isDatePickerVisible, setDatePickerVisible] = useState(false)
 
     useEffect(() => {
       if (id) {
         setTitle(card.title)
         setStatus(card.status)
         setNotes(card.notes)
+        setDueDate(new Date(card.dueDate))
       }
     }, [id, card])
 
     const handleSave = () => {
-      const cardData = { title, notes, status }
+      const cardData = { title, notes, status, dueDate: dueDate.toISOString() }
 
       if (id) {
         updateCard(id, cardData)
@@ -32,6 +37,29 @@ const CardEditScreen = ({ route, navigation }) => {
 
       navigation.goBack()
     }
+
+    const showDatePicker = () => {
+      setDatePickerVisible(true)
+    }
+
+    const hideDatePicker = () => {
+      setDatePickerVisible(false)
+    }
+
+    const handleConfirm = (date) => {
+      setDueDate(date)
+      hideDatePicker()
+    }
+
+    const formatDate = (date) => {
+      const day = date.getDate().toString().padStart(2, '0');
+      const month = (date.getMonth() + 1).toString().padStart(2, '0');
+      const year = date.getFullYear();
+      const hours = date.getHours().toString().padStart(2, '0');
+      const minutes = date.getMinutes().toString().padStart(2, '0');
+      return `${day}/${month}/${year} ${hours}:${minutes}`;
+  };
+
 
   return (
     <View style={styles.container}>
@@ -50,6 +78,15 @@ const CardEditScreen = ({ route, navigation }) => {
                 placeholder="Insira uma descrição..."
                 multiline
             />
+            <Text style={styles.label}>Data/Hora de Término:</Text>
+            <Button title="Escolhar Data" onPress={showDatePicker} color="#32cd320" />
+            <DateTimePickerModal
+              isVisible={isDatePickerVisible}
+              mode="datetime"
+              onConfirm={handleConfirm}
+              onCancel={hideDatePicker}
+            />
+            <Text style={styles.selectedDateLabel}>Data selecionada: {formatDate(dueDate)}</Text>
             <Text style={styles.label}>Status:</Text>
             <Picker
                 selectedValue={status}
@@ -74,6 +111,11 @@ const styles = StyleSheet.create({
       fontSize: 16,
       marginBottom: 5,
   },
+  selectedDateLabel: {
+      fontSize: 16,
+      marginBottom: 15,
+      color: '#555555',
+  },
   input: {
       fontSize: 14,
       borderWidth: 1,
@@ -81,7 +123,7 @@ const styles = StyleSheet.create({
       padding: 10,
       marginBottom: 15,
       borderRadius: 5,
-      width: '100%', // Certifique-se de que o picker preencha o espaço disponível
+      width: '100%',
   }
 });
 
